21d7968ecd689579c5fd2bee6125d387fd75a0fd
Václav <vaclav.volhejn@gmail.com>
2016-06-27 22:20:36 +0200

    Cleaned up Beige's code somewhat

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A collection of small Javascript (audio)visual pieces that I have created over the years. I do not consider these to be big enough to deserve their own repository.
 
 ## [Beige](https://iamwave.github.io/js-visual/beige/) (Aug 2014)
-A tiny, calming, interactive piece based on a recurring dream of mine.
+A tiny, calming, interactive piece based on a recurring dream of mine. Try dragging or holding the mouse.
 
 ## [Wolfram](https://iamwave.github.io/js-visual/wolfram/) (Feb 2016)
 Wolfram's cellular automata on a torus, with pretty colors. [Further instructions in the source.](wolfram/wolfram.js)
--- beige/beige.js
+++ beige/beige.js
@@ -1,32 +1,23 @@
 //Copyright (c) 2014-2016 Václav Volhejn
 
-//pomocné metody
-function createArray(length) {
-    var arr = new Array(length || 0),
-            i = length;
+var canvas = document.getElementById('beigeCanvas');
+var ctx = canvas.getContext("2d");
 
-    if (arguments.length > 1) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        while (i--)
-            arr[length - 1 - i] = createArray.apply(this, args);
-    }
+window.addEventListener('resize', function(event) {
+    canvas.width = ctx.canvas.clientWidth;
+    canvas.height = ctx.canvas.clientHeight;
+});
 
-    return arr;
-}
+canvas.width = ctx.canvas.clientWidth;
+canvas.height = ctx.canvas.clientHeight;
+
 function getPosition(event) {
-    var x = event.x;
-    var y = event.y;
-
-    var canvas = document.getElementById("beigeCanvas");
-
-    x -= canvas.offsetLeft;
-    y -= canvas.offsetTop;
-    var res = {};
-    res.x = x;
-    res.y = y;
-    return res;
+    return {
+        x: event.x - canvas.offsetLeft,
+        y: event.y - canvas.offsetTop
+    };
 }
-//konstanty
+
 var CONST = {
     BASE_SIZE: 20,
     RADIUS_INC: 0.1,
@@ -34,58 +25,48 @@ var CONST = {
     COLOR1: "white",
     COLOR2: "#F0F0C0",
     CURVE_EXPONENT: 2.3,
-    TRAIL_DELAY: 500,
-    MOVEMENT_REDUCTION_COEF: 10, //kdyz se pohne mys o X pixelu, cas se zkrati o X*k, kde k je toto
-    TRAIL_DURATION: 900
+    TRAIL_DELAY: 500, //a new wave is generated every TRAIL_DELAY ms
+    MOVEMENT_REDUCTION_COEF: 10, //moving the mouse by 1 pixel reduces the delay by this constant
+    TRAIL_DURATION: 900,
+    STATIC_DURATION: 1000,
 };
-//promìnné
-var canvas = document.getElementById('beigeCanvas');
-var ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 600;
+
 var mousePos = {x: 0, y: 0};
 var sources = [];
 var pressedSource = null;
 var trailMode = false;
-var lastTrailTime = 0; //kdy naposledy vytvarel kolecko
+var lastTrailTime = 0;
 var lastEvent = {x: 0, y: 0};
-//mousedown, mouseup, mousemove, mouseout and mouseover
+
 canvas.addEventListener('mousedown', function (event) {
     var newSource = getPosition(event);
     newSource.startTime = Date.now();
-    newSource.endTime = Date.now() + 1000;
+    newSource.endTime = Date.now() + CONST.STATIC_DURATION;
     sources.push(newSource);
     pressedSource = newSource;
     trailMode = false;
 }, false);
+
 canvas.addEventListener('mouseup', function () {
     pressedSource = null;
     size = CONST.BASE_SIZE;
     trailMode = false;
 }, false);
+
 canvas.addEventListener('mousemove', function (event) {
-    //console.log("moved!");
-    if (event.x === lastEvent.x && event.y === lastEvent.y) {
-        return;
-    }
-    var deltaX = lastEvent.x - event.x;
-    var deltaY = lastEvent.y - event.y;
-    var distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    if (event.x === lastEvent.x && event.y === lastEvent.y) return;
+
+    var distance = Math.hypot(lastEvent.x - event.x, lastEvent.y - event.y);
     mousePos = getPosition(event);
     if (pressedSource !== null){
         trailMode = true;
-        console.log(distance);
         lastTrailTime -= CONST.MOVEMENT_REDUCTION_COEF * distance;
     }
-    //lastTrailTime = Date.now();
     lastEvent = event;
 }, false);
 
-var update = function (delta) { //vrací, zda se má loop zopakovat
-
-    //ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+var update = function (delta) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     var cur;
     for (var i = 0; i < sources.length; i++) {
         if (Date.now() > sources[i].endTime) {
@@ -94,17 +75,12 @@ var update = function (delta) { //vrací, zda se má loop zopakovat
     }
     if (!trailMode) {
         if (pressedSource !== null) {
-            pressedSource.endTime = Date.now() + 1000;
+            pressedSource.endTime = Date.now() + CONST.STATIC_DURATION;
         }
     } else {
-        //console.log("trail " + (Date.now() - lastTrailTime - CONST.TRAIL_DELAY));
         if (Date.now() > lastTrailTime + CONST.TRAIL_DELAY) {
-
             lastTrailTime = Date.now();
             var newSource = JSON.parse(JSON.stringify(mousePos));
-
-            /*newSource.x = mousePos.x;
-             newSource.y = mousePos.y;*/
             newSource.startTime = Date.now();
             newSource.endTime = Date.now() + CONST.TRAIL_DURATION;
             sources.push(newSource);
@@ -120,9 +96,7 @@ var update = function (delta) { //vrací, zda se má loop zopakovat
 
         var size1 = curTime * CONST.TIME_TO_SIZE_COEF;
         var size2 = Math.pow(curTime, CONST.CURVE_EXPONENT) * coef2;
-//Math.max((curTime - totalTime / 2) * CONST.TIME_TO_SIZE_COEF * 2, 0);
-        //console.log(delta + ", " + size1);
-        ctx.fillStyle = "rgba(" + 100 + "," + 100 + "," + 100 + "," + 1 + ")";//"#FF0000";
+
         ctx.beginPath();
         ctx.arc(cur.x, cur.y, size1, 0, 2 * Math.PI, false);
         ctx.fillStyle = CONST.COLOR2;
