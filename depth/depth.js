86f3e1810acdba739be8a4769de449d211975cf8
Václav <vaclav.volhejn@gmail.com>
2016-08-31 18:22:46 +0200

    Cleaned up Depth's code

--- depth/depth.js
+++ depth/depth.js
@@ -1,76 +1,37 @@
-//pomocné metody
-function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length === 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-function createArray(length) {
-    var arr = new Array(length || 0),
-            i = length;
-
-    if (arguments.length > 1) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        while (i--)
-            arr[length - 1 - i] = createArray.apply(this, args);
-    }
-
-    return arr;
-}
-function getPosition(event) {
-    var x = event.x;
-    var y = event.y;
-
-    var canvas = document.getElementById("myCanvas");
-
-    x -= canvas.offsetLeft;
-    y -= canvas.offsetTop;
-    var res = {};
-    res.x = x;
-    res.y = y;
-    return res;
-}
-//konstanty
+//constants
 var CONST = {
-    WIDTH: 600,
-    HEIGHT: 600,
     ROTATION_COEF: 0.02,
-    ROTATE: true,
-    MIN_A_1: 10,
-    MIN_A_2: 15,
-    SIZE_COEF: 0.001, //0.7
+    MIN_A_1: 10, //triangles of this size are completely faded 
+    MIN_A_2: 15, //triangles of this size begin fading
+    SIZE_COEF: 0.001,
     MAX_SIZE: 900,
     DISPLAY_FPS: true,
 };
-function dist(x, y) {
-    return Math.sqrt(x * x + y * y);
-}
 
+//returns the height of the equilateral triangle with side a
 function getHeight(a) {
-    return Math.sqrt(0.75 * a * a);
+    return 0.866 * a; //0.866 = sqrt(0.75)
 }
 
-//proměnné
+//variables
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext("2d");
-canvas.width = CONST.WIDTH;
-canvas.height = CONST.HEIGHT;
+canvas.width = ctx.canvas.clientWidth;
+canvas.height = ctx.canvas.clientHeight;
+CONST.WIDTH = canvas.width;
+CONST.HEIGHT = canvas.height;
+CONST.MAX_SIZE = Math.max(CONST.WIDTH, CONST.HEIGHT) * 1.4;
+
 
 var size = CONST.HEIGHT;
 
 function drawFractal(x, y, a) {
-    if (a < CONST.MIN_A_1) {
-        //drawTriangle(x, y, a);
-        return;
-    }
-    if (y > dist(CONST.WIDTH, CONST.WIDTH)) {
-        return;
-    }
+    if (a < CONST.MIN_A_1) return;
+    if (y > Math.hypot(CONST.WIDTH, CONST.WIDTH)) return;
+
     if(a < CONST.MIN_A_2){
         var color = Math.round((a-CONST.MIN_A_1)/(CONST.MIN_A_2-CONST.MIN_A_1)*255);
-        ctx.fillStyle = rgbToHex(color, color, color);
+        ctx.fillStyle = "rgb("+color+","+color+","+color+")";
     } else{
         ctx.fillStyle = "white";
     }
@@ -98,29 +59,22 @@ function drawTriangle(x, y, a, up) {
     ctx.fill();
 }
 
-var update = function (delta) { //vrací, zda se má loop zopakovat
+var update = function (delta) {
     size *= 1 + (delta * CONST.SIZE_COEF);
     if (size > CONST.MAX_SIZE) {
         size = size / 2;
     }
 
-    if (CONST.ROTATE) {
-        ctx.translate(CONST.WIDTH / 2, CONST.HEIGHT / 2);
-        ctx.rotate(delta * CONST.ROTATION_COEF * Math.PI / 180);
-        ctx.fillStyle = "white";
-        ctx.fillRect(-CONST.WIDTH / 2, -CONST.HEIGHT / 2, CONST.WIDTH * 1.5, CONST.HEIGHT * 1.5);
-        ctx.translate(-CONST.WIDTH / 2, -CONST.HEIGHT / 2);
-    } else {
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-    }
-
+    ctx.translate(CONST.WIDTH / 2, CONST.HEIGHT / 2);
+    ctx.rotate(delta * CONST.ROTATION_COEF * Math.PI / 180);
+    //ctx.fillStyle = "white";
+    ctx.clearRect(-CONST.WIDTH / 2, -CONST.HEIGHT / 2, CONST.WIDTH * 1.5, CONST.HEIGHT * 1.5);
+    ctx.translate(-CONST.WIDTH / 2, -CONST.HEIGHT / 2);
+    
     ctx.fillStyle = "black";
     drawTriangle(CONST.WIDTH / 2, CONST.HEIGHT / 2, size);
-    ctx.fillStyle = "white";
+    //The fractal is made by drawing white triangles on a large black one.
     drawFractal(CONST.WIDTH / 2, CONST.HEIGHT / 2, size);
-    //ctx.fillRect(50, 20, 100, 50);
-
 };
 
 
